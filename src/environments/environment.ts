c7e95c87768caca3cd724cb40e51948621efd34b
Jose <jose.carreres.81@gmail.com>
2022-05-19 01:39:15 +0200

    API Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyARUxsw11txXtdJ0fFgvyPJMNSUeLoIlp4",
+    authDomain: "ionic-d98fa.firebaseapp.com",
+    projectId: "ionic-d98fa",
+    storageBucket: "ionic-d98fa.appspot.com",
+    messagingSenderId: "659217444270",
+    appId: "1:659217444270:web:7a247f29e29cc5ee99ec3f"
+  }
 };
 
 /*
