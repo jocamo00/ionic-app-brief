7f215e98fc3c32d7485f7c259ff9eb962c35271e
Jose <jose.carreres.81@gmail.com>
2022-05-19 20:17:50 +0200

    Login funcionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc:AuthService) { }
 
   ngOnInit() {
+  }
+
+  async onLogin(email, password) {
+    try {
+      const user = await this.authSvc.login(email.value, password.value);
+      if (user) {
+        console.log('User -->', user);
+      }
+
+    } catch (error) {
+      console.log('Error -->', error);
+    }
   }
 
 }
